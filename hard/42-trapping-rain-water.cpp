b2a05a0c7d7700ce6d2481b934e01ee53aaf7dcc
Gordon Nguyen <gordon-nguyen@outlook.com>
2024-12-08 22:26:03 -0600

    Rewrote loop logic

--- hard/42-trapping-rain-water.cpp
+++ hard/42-trapping-rain-water.cpp
@@ -6,49 +6,47 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         //check empty space from distance i to j with values of i = j
-        bool loop = true;
-        int current_height = 1, max_height = 1;
-        int first_index = 0;
+        int current_row = 1, max_row = 1;
+        int first_index = -1;
         int total_trap = 0;
-        int j = 0;
-        for (j; j < height.size(); j++) {
-            if ((height[j] >= current_height) && (first_index = 0)) {
-                first_index = j;
-                break;
-            }
-        }
+
         cout << "init first index: " << first_index << endl;
 
-        // Iterate all indexes in a row
-        while (first_index + 1 < height.size()) {
-            cout << "first index: " << first_index << endl;
-
-            // Iterate space between 2 "walls"
-            for (int i = first_index+1; i < height.size(); i++) {
+        for (current_row; current_row <= max_row; current_row++) {
+            cout << "row" << current_row << endl;
+            for (int i = 0; i < height.size(); i++) {
                 cout << "i" << i;
-                if ((height[i] >= current_height)) {
+                if ((height[i] >= current_row)) {
                     // Set max height to the highest value in the array
-                    if (height[i] > max_height) {
-                        max_height = height[i];
-                        cout << "max height " << max_height << endl;
+                    if (height[i] > max_row ) {
+                        max_row = height[i];
+                        cout << "max height " << max_row << endl;
                     }
-                    // Caclculate the space between index 1 and 2
-                    total_trap += i - first_index - 1;
-                    cout << endl << "total trap: " << total_trap << endl;
-                    first_index = i;
-                    cout << "first index in loop: " << first_index << endl;
-                    cout << "===============" << endl;
-                    break;
+                    // Check if index a is set
+                    if ((first_index == -1)) {
+                        first_index = i;
+                    }
+                    else {
+                        // Caclculate the space between index 1 and 2
+                        total_trap += i - first_index;
+                        cout << endl << "total trap: " << total_trap << endl;
+                        first_index = i;
+                        cout << "first index in loop: " << first_index << endl;
+                        cout << "===============" << endl;
+                        break;
+                    }
                 }
             }
+            // Unset first_index for next row
+            first_index = -1;
         }
-        // total_trap += empty_space
         return total_trap;
     }
 };
 
 int main() {
-    vector<int> v = {0,1,0,2,1,0,1,3,2,1,2,1};
+    vector<int> v1 = {0,1,0,2,1,0,1,3,2,1,2,1};
+    vector<int> v2 = {4,2,0,3,2,5};
     Solution sol;
-    cout << sol.trap(v) << endl;
+    cout << sol.trap(v2) << endl;
 }
